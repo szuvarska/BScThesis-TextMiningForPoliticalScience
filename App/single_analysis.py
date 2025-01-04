080c87e353e691719d60105fd321190b52429aee
szuvarska <01171269@pw.edu.pl>
2025-01-04 21:10:02 +0100

    Add sentence sentiment to single mode in app

--- App/single_analysis.py
+++ App/single_analysis.py
@@ -1,4 +1,5 @@
 import os
+from bs4 import BeautifulSoup
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pandas as pd
@@ -9,6 +10,7 @@ from NewsSentiment import TargetSentimentClassifier
 from NewsSentiment.customexceptions import TargetNotFoundException, TooLongTextException
 import re
 import warnings
+import nltk
 
 warnings.filterwarnings("ignore", category=FutureWarning, module="huggingface_hub.file_download")
 
@@ -58,35 +60,26 @@ def tsc_sentiment(sentence: str, target: str) -> str:
         return 'unknown'
 
 
-# def perform_ner_single_article(article_text: str):
-#     # Process the article text with the spacy model for visualization
-#     doc = nlp_spacy(article_text)
-#
-#     # Visualize the named entities using displacy
-#     html = displacy.render(doc, style="ent", jupyter=False,
-#                            options={"ents": ["ORG", "PERSON", "GPE", "LOC", "NORP", "EVENT"]})
-#
-#     # Add the legend to the HTML output
-#     legend_html = "<div class='legend'><h3>Entity Legend</h3><ul>"
-#     for entity, description in entity_legend.items():
-#         legend_html += f"<li><strong>{entity}</strong>: {description}</li>"
-#     legend_html += "</ul></div>"
-#
-#     # Combine the legend and the NER visualization
-#     full_html = legend_html + html
-#
-#     return full_html
-
-
 def analyse_single_article(article_text: str):
+    positive_emoji = "emoji_1"
+    negative_emoji = "emoji_2"
+    neutral_emoji = "emoji_3"
+    sentences = [sent.text.strip() for sent in nlp(article_text).sents]
+
+    # Analyze sentiment for each sentence
+    for i, sentence in enumerate(sentences):
+        sentiment = vader_sentiment(sentence)
+        sentiment_emoji = positive_emoji if sentiment == "positive" else negative_emoji if sentiment == "negative" else neutral_emoji
+        sentences[i] = sentence + ' ' + sentiment_emoji
+
+    print(sentences)
+
+    modified_text = " ".join(sentences)
     # Process the article text with the spacy model for visualization
-    doc = nlp(article_text)
+    doc = nlp(modified_text)
 
     # Extract named entities
     entities = [(ent.text, ent.label_) for ent in doc.ents if ent.label_ in entity_legend.keys()]
-
-    # Split article into sentences
-    sentences = [sent.text for sent in doc.sents]
 
     # Analyze sentiment for each entity
     entity_sentiments = []
@@ -97,44 +90,57 @@ def analyse_single_article(article_text: str):
                 entity_sentiments.append((entity, label, sentiment))
                 break
 
-    # Analyze sentiment for each sentence
-    sentence_sentiments = []
-    for sentence in sentences:
-        sentiment = vader_sentiment(sentence)
-        sentence_sentiments.append((sentence, sentiment))
-
     # Visualize the named entities using displacy
     options = {"ents": list(entity_legend.keys())}
     html = displacy.render(doc, style="ent", jupyter=False, options=options)
+    html = html.replace(
+        "emoji_1",
+        '<a href="https://img.icons8.com/ios-filled/50/00FF00/happy--v1.png" target="_blank">'
+        '<img width="16" height="16" src="https://img.icons8.com/ios-filled/50/00FF00/happy--v1.png" '
+        'alt="Positive sentiment" style="background-color: black; border-radius: 50%; padding: 0px;"/>'
+        '<span class="tooltip"><b>Positive</b> sentence</span>'
+        '</a>'
+    )
+    html = html.replace(
+        "emoji_2",
+        '<a href="https://img.icons8.com/ios-filled/50/FF0000/sad.png" target="_blank">'
+        '<img width="16" height="16" src="https://img.icons8.com/ios-filled/50/FF0000/sad.png" '
+        'alt="Negative sentiment" style="background-color: black; border-radius: 50%; padding: 0px;"/>'
+        '<span class="tooltip"><b>Negative</b> sentence</span>'
+        '</a>'
+    )
+    html = html.replace(
+        "emoji_3",
+        '<a href="https://img.icons8.com/ios-filled/50/FCC419/neutral-emoticon--v1.png" target="_blank">'
+        '<img width="16" height="16" src="https://img.icons8.com/ios-filled/50/FCC419/neutral-emoticon--v1.png" '
+        'alt="Neutral sentiment" style="background-color: black; border-radius: 50%; padding: 0px;"/>'
+        '<span class="tooltip"><b>Neutral</b> sentence</span>'
+        '</a>'
+    )
 
     # Add the legend to the HTML output
     legend_html = "<div class='legend'><h3>Entity Legend</h3><ul>"
     for entity, description in entity_legend.items():
         legend_html += f"<li><strong>{entity}</strong>: {description}</li>"
     legend_html += "</ul></div>"
-
-    # Add sentiment analysis results to the HTML output
-    sentiment_html = "<div class='sentiment-analysis'><h3>Sentiment Analysis</h3><ul>"
-    for entity, label, sentiment in entity_sentiments:
-        sentiment_html += f"<li><strong>{entity} ({label})</strong>: {sentiment}</li>"
-    sentiment_html += "</ul><h4>Sentence Sentiments</h4><ul>"
-    for sentence, sentiment in sentence_sentiments:
-        sentiment_html += f"<li>{sentence}: {sentiment}</li>"
-    sentiment_html += "</ul></div>"
+    legend_html += "<div class='legend'><h3>Sentiment Legend</h3><ul>"
+    legend_html += f'<li><strong style="color: green;">Green</strong>: Positive sentiment</li>'
+    legend_html += f'<li><strong style="color: #FFBF00;">Yellow</strong>: Neutral sentiment</li>'
+    legend_html += f'<li><strong style="color: red;">Red</strong>: Negative sentiment</li>'
 
     # Combine the legend, NER visualization, and sentiment analysis results
-    full_html = legend_html + html + sentiment_html
+    full_html = legend_html + html
 
     # Add inline styles for entity colors based on sentiment
     for entity, label, sentiment in entity_sentiments:
         color = "green" if sentiment == "positive" else "red" if sentiment == "negative" else "yellow"
         tooltip_value = f'<b>Value:</b> {entity}<br><b>Entity type:</b> {entity_legend[label]}<br><b>Sentiment:</b> {sentiment}'
-        # tooltip_value = sentiment
         tooltip = f'><span class="tooltip">{tooltip_value}</span>'
         title = f'"<b>Value:</b> {entity}<br><b>Entity type:</b> {entity_legend[label]}<br><b>Sentiment:</b> {sentiment}">'
         pattern = fr'(<mark class="entity" style="background: )#[a-fA-F0-9]+(;[^>]*?>\s*{re.escape(entity)})'
         full_html = re.sub(pattern, lambda
             match: f'{match.group(1)}{color}{match.group(2).replace(">", tooltip, 1)}', full_html)
+
     return full_html
 
 
