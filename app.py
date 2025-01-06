2cdd4454a0b99035b2e9fa66c4769a1b5ada86d1
szuvarska <01171269@pw.edu.pl>
2025-01-06 19:53:36 +0100

    Fromatting about module

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ about_module = ui.tags.div(
         ),
         class_="main-left-container"
     ),
-    class_="main-container"
+    class_="main-container about-module"
 )
 
 page_dependencies = ui.head_content(
@@ -916,41 +916,56 @@ def server(input, output, session):
     @render.ui
     def introduction_content():
         if introduction_visible.get():
-            return ui.HTML("""<p>This application provides an analysis of articles from the Global Times. It includes 
-            various modules:</p> <ul> <li><strong>Single:</strong> Analyze a single article.</li> 
-            <li><strong>Double:</strong> Compare two articles.</li> <li><strong>All:</strong> Perform exploratory 
-            data analysis on a dataset of articles.</li> </ul>""")
+            return ui.HTML("""
+            <div class="collapsible-section-content">
+                <h3>This application provides an analysis of articles from the Global Times. It includes various modules:</h3>
+                <br>
+                <ul>
+                    <li><strong>Single:</strong> Analyze a single article.</li>
+                    <li><strong>Double:</strong> Compare two articles.</li>
+                    <li><strong>All:</strong> Perform exploratory data analysis on a dataset of articles.</li>
+                </ul>
+            </div>
+        """)
         return ui.div()
 
     @output
     @render.ui
     def guide_content():
         if guide_visible.get():
-            return ui.HTML("""<p>Instructions on how to use the application:</p> <ul> <li><strong>Select a 
-            file:</strong> Use the dropdown to select a file from the list.</li> <li><strong>Upload a file:</strong> 
-            Click the "UPLOAD ARTICLE" button to upload a new file.</li> <li><strong>View more/less:</strong> Click 
-            the "View more" button to expand the text, and "View less" to collapse it.</li> <li><strong>Hide/Show 
-            Menu:</strong> Click the "Hide Menu" button to hide the right container, and "Show Menu" to display it 
-            again.</li> <li><strong>Select Dataset:</strong> Use the dropdown to select a dataset for analysis.</li> 
-            <li><strong>Select Sentiment:</strong> Use the dropdown to filter by sentiment (Positive, Neutral, 
-            Negative).</li> <li><strong>Select Entity Type:</strong> Use the dropdown to filter by entity type (
-            Person, Organisation, Location, Miscellaneous).</li> <li><strong>Select Sentiment Model:</strong> Use the 
-            dropdown to select a sentiment model (TSC, VADER).</li> <li><strong>Number of Words in Word 
-            Cloud:</strong> Use the numeric input to specify the number of words in the word cloud.</li> 
-            <li><strong>Select Part of Speech:</strong> Use the dropdown to filter by part of speech.</li> 
-            <li><strong>Filter Words:</strong> Use the text input to filter words (comma-separated).</li> 
-            <li><strong>N-gram Number:</strong> Use the numeric input to specify the n-gram number.</li> </ul>""")
+            return ui.HTML("""
+                <div class="collapsible-section-content">
+                    <h3>Instructions on how to use the application:</h3><br>
+                    <ul>
+                        <li><strong style="color: #333;">Select a file:</strong> Use the dropdown to select a file from the list.</li>
+                        <li><strong style="color: #333;">Upload a file:</strong> Click the "UPLOAD ARTICLE" button to upload a new file.</li>
+                        <li><strong style="color: #333;">View more/less:</strong> Click the "View more" button to expand the text, and "View less" to collapse it.</li>
+                        <li><strong style="color: #333;">Hide/Show Menu:</strong> Click the "Hide Menu" button to hide the right container, and "Show Menu" to display it again.</li>
+                        <li><strong style="color: #333;">Select Dataset:</strong> Use the dropdown to select a dataset for analysis.</li>
+                        <li><strong style="color: #333;">Select Sentiment:</strong> Use the dropdown to filter by sentiment (Positive, Neutral, Negative).</li>
+                        <li><strong style="color: #333;">Select Entity Type:</strong> Use the dropdown to filter by entity type (Person, Organisation, Location, Miscellaneous).</li>
+                        <li><strong style="color: #333;">Select Sentiment Model:</strong> Use the dropdown to select a sentiment model (TSC, VADER).</li>
+                        <li><strong style="color: #333;">Number of Words in Word Cloud:</strong> Use the numeric input to specify the number of words in the word cloud.</li>
+                        <li><strong style="color: #333;">Select Part of Speech:</strong> Use the dropdown to filter by part of speech.</li>
+                        <li><strong style="color: #333;">Filter Words:</strong> Use the text input to filter words (comma-separated).</li>
+                        <li><strong style="color: #333;">N-gram Number:</strong> Use the numeric input to specify the n-gram number.</li>
+                    </ul>
+                </div>
+            """)
         return ui.div()
 
     @output
     @render.ui
     def authors_content():
         if authors_visible.get():
-            return ui.HTML("""<p><strong>Authors:</strong> Łukasz Grabarski & Marta Szuwarska (Warsaw University of 
-            Technology)</p> <p><strong>Supervisor:</strong> Dr. Anna Wróblewska (Warsaw University of Technology)</p> 
-            <p><strong>Co-supervisor:</strong> Prof. Agnieszka Kaliska (Adam Mickiewicz University in Poznań)</p> 
-            <p><strong>Co-operations:</strong> Prof. Anna Rudakowska (Tamkang University in Taiwan), Dr. Daniel Dan (
-            Modul University in Vienna)</p>""")
+            return ui.HTML("""
+                <div class="collapsible-section-content">
+                    <p><strong style="color: #333;">Authors:</strong> Łukasz Grabarski & Marta Szuwarska (Warsaw University of Technology)</p>
+                    <p><strong style="color: #333;">Supervisor:</strong> Dr. Anna Wróblewska (Warsaw University of Technology)</p>
+                    <p><strong style="color: #333;">Co-supervisor:</strong> Prof. Agnieszka Kaliska (Adam Mickiewicz University in Poznań)</p>
+                    <p><strong style="color: #333;">Co-operations:</strong> Prof. Anna Rudakowska (Tamkang University in Taiwan), Dr. Daniel Dan (Modul University in Vienna)</p>
+                </div>
+            """)
         return ui.div()
 
 
