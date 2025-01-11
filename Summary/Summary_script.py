d9971440c8eef1403cf7f10c1653f45146f2b40b
LukaszGrabarski <lukasgrabarski@gmail.com>
2025-01-11 18:29:44 +0100

    Add article summarization

---
+++ Summary/Summary_script.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+summarizer = None
+
+def download_model():
+    global summarizer
+    print("Downloading 'facebook/bart-large-cnn'...")
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    print("Model downloaded!")
+    
+def summarize_text(text):
+    global summarizer
+    summary = summarizer(text, max_length=150, min_length=30, do_sample=False)
+    return summary[0]["summary_text"]
